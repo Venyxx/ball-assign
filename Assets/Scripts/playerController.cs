8f5d7a10e24d914a12076f9c7fd3023a77b19beb
Venyxx <scottvanessa01@gmail.com>
2021-07-11 23:28:52 -0400

    July 11, 2021 11:28 PM

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,14 +5,20 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     void onMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+
+    void fixedUpdate()
+    {
+        rb.AddForce{movementVector};
     }
 }
